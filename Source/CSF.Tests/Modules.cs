be15d987ea67a12fb35e11ab34b8cae4a5be6c5b
Rozen <t.a.denboef@gmail.com>
2022-10-24 19:25:47 +0200

    Scrap buildresult

--- Source/CSF.Tests/Modules.cs
+++ Source/CSF.Tests/Modules.cs
@@ -8,8 +8,10 @@ namespace CSF.Tests
 {
     internal class Modules : ModuleBase<CommandContext>
     {
-        [Command("")]
+        [Command("help")]
+#pragma warning disable CA1822 // Mark members as static
         public IResult Help()
+#pragma warning restore CA1822 // Mark members as static
         {
             Console.WriteLine("Success");
 
