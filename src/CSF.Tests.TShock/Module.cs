7089d721a9c72e51790bf32164960e11c1c3ea28
Rozen <t.a.denboef@gmail.com>
2022-11-23 12:56:38 +0100

    Final reorganization

---
+++ src/CSF.Tests.TShock/Module.cs
@@ -0,0 +1,18 @@
+﻿using CSF.TShock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF.Tests.TShock
+{
+    [RequirePermission("plugin")]
+    public class Module : TSModuleBase<TSCommandContext>
+    {
+        [RequirePermission("test")]
+        [Command("test", "t")]
+        public void Test()
+            => Success("tested!");
+    }
+}
