8781279301c9333dcc42f94af1c7da51089db59f
Rozen <t.a.denboef@gmail.com>
2022-11-23 17:00:39 +0100

    Cleanup, package data

--- src/CSF.Hosting/Implementations/CommandStreamListener.cs
+++ src/CSF.Hosting/Implementations/CommandStreamListener.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSF.Hosting
 {
--- src/CSF.TShock/Server/ServerInfo.cs
+++ src/CSF.TShock/Server/ServerInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using TShockAPI;
+﻿using TShockAPI;
 
 namespace CSF.TShock
 {
