97d4bb9330cc4603ccabcae77ea56aba108628a6
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2023-01-12 13:26:48 +0100

    XML on exceptions

--- src/CSF.Core/Utils/Exceptions/MemberUnpreparedException.cs
+++ src/CSF.Core/Utils/Exceptions/MemberUnpreparedException.cs
@@ -5,14 +5,24 @@ using System.Text;
 
 namespace CSF
 {
+    /// <summary>
+    ///     Thrown when a member is not ready to be executed.
+    /// </summary>
     public sealed class MemberUnpreparedException : Exception
     {
+        /// <summary>
+        ///     Creates a new <see cref="MemberUnpreparedException"/> with base values.
+        /// </summary>
         public MemberUnpreparedException()
             : base("Called an unprepared operation in that was not ready to execute.")
         {
 
         }
 
+        /// <summary>
+        ///     Creates a new <see cref="MemberUnpreparedException"/> with base values.
+        /// </summary>
+        /// <param name="innerException">The inner exception that led to this exception.</param>
         public MemberUnpreparedException(Exception innerException)
             : base("Called an unprepared operation in that was not ready to execute.", innerException)
         {
--- src/CSF.Core/Utils/Exceptions/MissingValueException.cs
+++ src/CSF.Core/Utils/Exceptions/MissingValueException.cs
@@ -4,14 +4,28 @@ using System.Text;
 
 namespace CSF
 {
+    /// <summary>
+    ///     Thrown when a value was found missing on evaluation.
+    /// </summary>
     public sealed class MissingValueException : ArgumentException
     {
-        public MissingValueException(string message, string paramName, Exception exception)
-            : base(message, paramName, exception)
+        /// <summary>
+        ///     Creates a new <see cref="MissingValueException"/> with provided values.
+        /// </summary>
+        /// <param name="message">The error message that occurred from this exception.</param>
+        /// <param name="paramName">The parameter name that was found to have missing values.</param>
+        /// <param name="innerException">The inner exception that led to this exception.</param>
+        public MissingValueException(string message, string paramName, Exception innerException)
+            : base(message, paramName, innerException)
         {
 
         }
 
+        /// <summary>
+        ///     Creates a new <see cref="MissingValueException"/> with provided values.
+        /// </summary>
+        /// <param name="message">The error message that occurred from this exception.</param>
+        /// <param name="paramName">The parameter name that was found to have missing values.</param>
         public MissingValueException(string message, string paramName) 
             : base(paramName, message)
         {
